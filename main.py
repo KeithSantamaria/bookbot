f74b1a0e02247ae2400976eb65cf08d155834d85
KeithSantamaria <keith.santamaria715@gmail.com>
2025-04-19 03:19:09 -0500

    Created word count func

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+def count_words_from_book(book):
+    count = 0
+    words = book.split()
+    for word in words:
+        count += 1
+    return count
 
 def main():
     book = get_book_text("books/frankenstein.txt")
-    print(book)
+    num_words = count_words_from_book(book)
+    print(f"{num_words} words found in the document")
+
 main()
